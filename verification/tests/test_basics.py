abd59a664145350f2cfc1612d3f6ed9f0cf3abc2
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-07-09 10:18:45 +1200

    Change structure

---
+++ verification/tests/test_basics.py
@@ -0,0 +1,20 @@
+OPTIONS = {
+}
+
+TESTS = [
+    {
+        "input": [1, 2, 3],
+        "answer": 5,
+        "explanation": "???"
+    },
+    {
+        "input": [[1], [2, 3]],
+        "answer": 3.23232,
+        "explanation": "???"
+    },
+    {
+        "input": 4.555,
+        "answer": "dsdsds",
+        "explanation": "???"
+    }
+]
