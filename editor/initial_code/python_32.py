b180ef7a1f5370c9d13f09a1439e0e31a2c35221
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-07-22 16:53:15 +1200

    Fix initial codes

---
+++ editor/initial_code/python_32.py
@@ -0,0 +1,8 @@
+def checkio(data):
+
+    #replace this for solution
+    return None
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    pass
