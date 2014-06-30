417f8a2af5be79dffadef8ed8fb35ae07d4bfd35
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-06-30 13:05:33 +1200

    hint template

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -43,7 +43,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             var fname = 'checkio';
 
             var checkioInput = data.in;
-            var checkioInputStr = ' ' + fname + '(' + JSON.stringify(checkioInput)  + ')';
+            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput)  + ')';
 
             var failError = function(dError) {
                 $content.find('.call').html('Fail: ' + checkioInputStr);
