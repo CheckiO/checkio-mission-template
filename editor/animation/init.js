e457d37b22325072579a276507704427f6e2faa7
oduvan <a.lyabah@checkio.org>
2017-01-27 21:44:35 +0200

    template for a new version of CheckiO

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,157 +1,17 @@
 //Dont change it
-requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
-    function (ext, $, Raphael, Snap) {
+//Dont change it
+requirejs(['ext_editor_io', 'jquery_190'],
+    function (extIO, $) {
+        
+        var $tryit;
 
-        var cur_slide = {};
-
-        ext.set_start_game(function (this_e) {
-        });
-
-        ext.set_process_in(function (this_e, data) {
-            cur_slide = {};
-            cur_slide["in"] = data[0];
-            this_e.addAnimationSlide(cur_slide);
-        });
-
-        ext.set_process_out(function (this_e, data) {
-            cur_slide["out"] = data[0];
-        });
-
-        ext.set_process_ext(function (this_e, data) {
-            cur_slide.ext = data;
-        });
-
-        ext.set_process_err(function (this_e, data) {
-            cur_slide['error'] = data[0];
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
-        });
-
-        ext.set_animate_success_slide(function (this_e, options) {
-            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div></div></div>'));
-            this_e.setAnimationHeight(115);
-        });
-
-        ext.set_animate_slide(function (this_e, data, options) {
-            var $content = $(this_e.setHtmlSlide(ext.get_template('animation'))).find('.animation-content');
-            if (!data) {
-                console.log("data is undefined");
-                return false;
+        var io = new extIO({
+            multipleArguments: true,
+            functions: {
+                js: 'sumTwo',
+                python: 'sum_two'
             }
-
-            //YOUR FUNCTION NAME
-            var fname = 'checkio';
-
-            var checkioInput = data.in;
-            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput) + ')';
-
-            var failError = function (dError) {
-                $content.find('.call').html(checkioInputStr);
-                $content.find('.output').html(dError.replace(/\n/g, ","));
-
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
-                $content.find('.answer').remove();
-                $content.find('.explanation').remove();
-                this_e.setAnimationHeight($content.height() + 60);
-            };
-
-            if (data.error) {
-                failError(data.error);
-                return false;
-            }
-
-            if (data.ext && data.ext.inspector_fail) {
-                failError(data.ext.inspector_result_addon);
-                return false;
-            }
-
-            $content.find('.call').html(checkioInputStr);
-            $content.find('.output').html('Working...');
-
-
-            if (data.ext) {
-                var rightResult = data.ext["answer"];
-                var userResult = data.out;
-                var result = data.ext["result"];
-                var result_addon = data.ext["result_addon"];
-
-                //if you need additional info from tests (if exists)
-                var explanation = data.ext["explanation"];
-                $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
-                if (!result) {
-                    $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
-                    $content.find('.answer').addClass('error');
-                    $content.find('.output').addClass('error');
-                    $content.find('.call').addClass('error');
-                }
-                else {
-                    $content.find('.answer').remove();
-                }
-            }
-            else {
-                $content.find('.answer').remove();
-            }
-
-
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
-
-
-            this_e.setAnimationHeight($content.height() + 60);
-
         });
-
-        //This is for Tryit (but not necessary)
-//        var $tryit;
-//        ext.set_console_process_ret(function (this_e, ret) {
-//            $tryit.find(".checkio-result").html("Result<br>" + ret);
-//        });
-//
-//        ext.set_generate_animation_panel(function (this_e) {
-//            $tryit = $(this_e.setHtmlTryIt(ext.get_template('tryit'))).find('.tryit-content');
-//            $tryit.find('.bn-check').click(function (e) {
-//                e.preventDefault();
-//                this_e.sendToConsoleCheckiO("something");
-//            });
-//        });
-
-
-        function SVG(dom) {
-            var colorOrange4 = "#a83901";
-            var colorOrange3 = "#ca4701";
-            var colorOrange2 = "#fa5e09";
-            var colorOrange1 = "#Ff935b";
-
-            var colorBlue4 = "#115d81";
-            var colorBlue3 = "#2b95c5";
-            var colorBlue2 = "#9ad0ec";
-            var colorBlue1 = "#dceaf1";
-
-            var colorGrey4 = "#000000";
-            var colorGrey3 = "#373737";
-            var colorGrey2 = "#7e7e7e";
-            var colorGrey1 = "#cecece";
-
-            var colorWhite = "#FFFFFF";
-
-            var paper;
-
-            this.draw = function() {};
-
-            this.animate = function(){};
-        }
-
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
-
-
+        io.start();
     }
 );
--- verification/referee.py
+++ verification/referee.py
@@ -30,7 +30,6 @@ from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
 from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
 
@@ -38,12 +37,13 @@ api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
-        cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
+        function_name={
+            "python": "sum_two",
+            "js": "sumTwo"
         },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
+        cover_code={
+            'python-27': cover_codes.unwrap_args,
+            'python-3': cover_codes.unwrap_args,
+            'js-node': cover_codes.js_unwrap_args
+        }
     ).on_ready)
